6f6a555badbce848e9c6f7b6a1160ac0d910b243
zwr520zdf <704853783@qq.com>
2019-09-05 17:12:50 +0800

    springboot搭建mybatis

---
+++ spring-boot-08-data-mybatis/src/main/java/com/mengxuegu/springboot/SpringBoot08DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.mengxuegu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot08DataMybatisApplication.class, args);
+    }
+
+}
